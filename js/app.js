b6d692d868c6b8fadb7df996b850d05d2d77943e
lgsmd <584588192@qq.com>
2019-08-01 20:02:07 +0800

    give enemy speed for random

--- js/app.js
+++ js/app.js
@@ -2,23 +2,25 @@
 var Enemy = function() {
     // 要应用到每个敌人的实例的变量写在这里
     // 我们已经提供了一个来帮助你实现更多
-    this.x = -95;
+    this.x = -Math.round(Math.random() * 1200 - 95);
     this.y = 60;
-    // 敌人的图片，用一个我们提供的工具函数来轻松的加载文件
-    this.sprite = 'images/enemy-bug.png';
-};
+    this.speed = Math.ceil(Math.random() * 7) * 100;
+            // 敌人的图片，用一个我们提供的工具函数来轻松的加载文件
+            this.sprite = 'images/enemy-bug.png';
+    };
 
+// let speed = Math.ceil(Math.random() * 7) * 100;
 // 此为游戏必须的函数，用来更新敌人的位置
 // 参数: dt ，表示时间间隙
-Enemy.prototype.update = function(dt) {
-    // 你应该给每一次的移动都乘以 dt 参数，以此来保证游戏在所有的电脑上
-    // 都是以同样的速度运行的
-    this.x += 1;
-    if(this.x <= 505){
-        this.x += 1 * dt;
-    }else {
-        this.x = -95;
-    }
+    Enemy.prototype.update = function(dt) {
+        // 你应该给每一次的移动都乘以 dt 参数，以此来保证游戏在所有的电脑上
+        // 都是以同样的速度运行的
+        if(this.x < 505){
+            this.x += this.speed * dt;
+        }else {
+            this.x = -Math.round(Math.random() * 900) - 95;
+            this.speed = Math.ceil(Math.random() * 7) * 100;
+        }
 };
 
 // 此为游戏必须的函数，用来在屏幕上画出敌人，
@@ -54,9 +56,12 @@ let player = new Player();
 let enemyA = new Enemy();
 let enemyB = new Enemy();
 let enemyC = new Enemy();
-enemyB.y = 143;
-enemyC.y = 226;
-const allEnemies = [enemyA,enemyB,enemyC];
+let enemyD = new Enemy();
+let enemyE = new Enemy();
+let enemyF = new Enemy();
+enemyB.y = enemyE.y = 143;
+enemyC.y = enemyF.y = 226;
+const allEnemies = [enemyA,enemyB,enemyC,enemyD,enemyE,enemyF];
 
 // 这段代码监听游戏玩家的键盘点击事件并且代表将按键的关键数字送到 Player.handleInput()
 // 方法里面。你不需要再更改这段代码了。
