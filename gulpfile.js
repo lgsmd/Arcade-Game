e505b72e02e6451e2def6fb7511355e096f98342
lgsmd <584588192@qq.com>
2019-08-06 16:45:40 +0800

    about brwoser-Sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,8 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
-gulp.task('default', function () {
-    console.log('Hello, Gulp!');
-})
+
+gulp.task('default', defaultTask)
 gulp.task('styles', function () {
     gulp.src('sass/**/*.scss')
         .pipe(sass().on('error', sass.logError))
@@ -12,3 +11,14 @@ gulp.task('styles', function () {
         }))
         .pipe(gulp.dest('./css'));
 });
+
+//创建浏览器同步对象并初始化服务器
+function defaultTask(done) {
+    var browserSync = require('browser-sync').create();
+    browserSync.init({
+        //这里的意思是含有这个文件的目录地址，可以将目录更改
+        server: "./"
+    });
+    browserSync.stream();
+    done();
+}
