532aeedebc589e0b343ab4cba9380d0ef4edbae0
lgsmd <584588192@qq.com>
2019-08-07 16:18:25 +0800

    study gulp-concat

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
+var concat = require('gulp-concat');
 
-gulp.task('default', defaultTask)
+//gulp.task('default', defaultTask)
 gulp.task('styles', function () {
     gulp.src('sass/**/*.scss')
         .pipe(sass().on('error', sass.logError))
@@ -13,12 +14,23 @@ gulp.task('styles', function () {
 });
 
 //创建浏览器同步对象并初始化服务器
-function defaultTask(done) {
-    var browserSync = require('browser-sync').create();
-    browserSync.init({
-        //这里的意思是含有这个文件的目录地址，可以将目录更改
-        server: "./"
-    });
-    browserSync.stream();
-    done();
-}
+// function defaultTask(done) {
+//     var browserSync = require('browser-sync').create();
+//     browserSync.init({
+//         //这里的意思是含有这个文件的目录地址，可以将目录更改
+//         server: "./"
+//     });
+//     browserSync.stream();
+//     done();
+// }
+gulp.task('scripts', function () {
+    gulp.src('js/**/*.js')
+        .pipe(concat('all.js'))
+        .pipe(gulp.dest('dist/js'));
+})
+
+gulp.task('scripts-dist', function () {
+    gulp.src('js/**/*.js')
+        .pipe(concat('all.js'))
+        .pipe(gulp.dest('dist/js'));
+})
